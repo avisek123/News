339b8339ce08acbe168b12dc80bcc49a50b88d70
avisek123 <mrperfectavi89@gmail.com>
2021-07-11 03:33:04 +0530

    display story

--- src/Pages/ViewStory.js
+++ src/Pages/ViewStory.js
@@ -1,7 +1,35 @@
+import {
+  Container,
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import { useStory } from "../Hooks";
+const useStyles = makeStyles((theme) => ({
+  card: {
+    marginTop: theme.spacing(10),
+  },
+}));
 const ViewStory = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const { data } = useStory();
+  const res = data.filter((item) => item?.storyID === id)?.[0];
+
+  const classes = useStyles();
+  return (
+    <Container className={classes.card}>
+      <Card>
+        <CardHeader title={res?.title} />
+        <CardContent>
+          <Typography>{res?.description}</Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default ViewStory;
